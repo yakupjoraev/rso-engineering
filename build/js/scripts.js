035c437c3fa7a5d833595ba9df01b243a64d4f58
Yakup <yakupjoraev@gmail.com>
2023-12-19 13:16:07 +0300

    added fixes

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -259,3 +259,20 @@ function map() {
 }
 
 map();
+
+
+//плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
